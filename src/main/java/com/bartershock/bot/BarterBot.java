4ceca0ca1c58d5d5fae0227fb3266be187ce175f
CThaw90 <cthaw@mediaocean.com>
2016-09-12 17:19:39 -0400

    Able to get past the login portal

--- src/main/java/com/bartershock/bot/BarterBot.java
+++ src/main/java/com/bartershock/bot/BarterBot.java
@@ -2,13 +2,15 @@ package com.bartershock.bot;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.CookieManager;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.HttpMethod;
 import com.gargoylesoftware.htmlunit.HttpWebConnection;
-import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
 import com.gargoylesoftware.htmlunit.Page;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.WebClientOptions;
@@ -21,24 +23,28 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
 import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
 import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+import com.gargoylesoftware.htmlunit.util.FalsifyingWebConnection;
+import com.gargoylesoftware.htmlunit.util.NameValuePair;
 
 public class BarterBot {
 
 	public static void main(String[] args) throws FailingHttpStatusCodeException, MalformedURLException, IOException, InterruptedException {
 		System.out.println("Starting Bot Crawler");
-		// System.getProperties().put("org.apache.commons.logging.simplelog.defaultlog", "trace");
 		
 		WebClient webClient = new WebClient(BrowserVersion.CHROME);
 		WebClientOptions webClientOptions = webClient.getOptions();
 		CookieManager cookieManager = webClient.getCookieManager();
 		
-		webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+		// webClient.setAjaxController(new NicelyResynchronizingAjaxController());
 
-		webClientOptions.setJavaScriptEnabled(false);
+		webClientOptions.setThrowExceptionOnScriptError(false);
+		webClientOptions.setJavaScriptEnabled(true);
 		webClientOptions.setCssEnabled(false);
 		webClientOptions.setRedirectEnabled(true);
 		webClientOptions.setUseInsecureSSL(true);
 		cookieManager.setCookiesEnabled(true);
+		
+		new BarterBot.InterceptWebConnection(webClient);
 		
 		final HtmlPage page = webClient.getPage("https://wellsfargo.com");
 		final HtmlForm form = (HtmlForm) page.getElementById("frmSignon");
@@ -50,14 +56,20 @@ public class BarterBot {
 		username.setValueAttribute("username");
 		password.setValueAttribute("password");
 		
-//		System.out.println(username.asXml());
-//		System.out.println(password.asXml());
-//		System.out.println(submit.asXml());
-//		System.out.println(form.asXml());
-		
 		webClient.setWebConnection(new HttpWebConnection (webClient) {
 			public WebResponse getResponse(WebRequest request) throws IOException {
-				if (request.getHttpMethod().equals(HttpMethod.POST)) {
+				if (request.getHttpMethod().equals(HttpMethod.POST) && request.getRequestBody() == null) {
+					List<NameValuePair> requestParameters = new ArrayList<NameValuePair>(request.getRequestParameters());
+					StringBuilder requestBodyConversion = new StringBuilder();
+					String appender = "";
+					request.setRequestParameters(null);
+					for (NameValuePair parameter : requestParameters) {
+						String name = parameter.getName(), value = parameter.getValue();
+						requestBodyConversion.append(appender).append(name).append("=").append(URLEncoder.encode(value, "UTF-8"));
+						appender = "&";
+					}
+					
+					request.setRequestBody(requestBodyConversion.toString());
 					System.out.println("HTTP METHOD: " + request.getHttpMethod());
 					System.out.println("URL: " + request.getUrl());
 					System.out.println("ENCODING: " + request.getEncodingType());
@@ -72,13 +84,25 @@ public class BarterBot {
 		});
 		
 		Page responsePage = submit.click();
-//		WebRequest webRequest = responsePage.getWebResponse().getWebRequest();
-//		System.out.println(webRequest.getHttpMethod());
-//		System.out.println(webRequest.getEncodingType());
-//		System.out.println(webRequest.getRequestBody());
-//		System.out.println(responsePage.getWebResponse().getWebRequest());
 		final HtmlPage resultPage = HTMLParser.parseHtml(responsePage.getWebResponse(), webClient.getCurrentWindow());
 		System.out.println(resultPage.getUrl());
+		
 		webClient.close();
+	}
+	
+	static class InterceptWebConnection extends FalsifyingWebConnection {
+		
+		public InterceptWebConnection(WebClient webClient) throws IllegalArgumentException {
+			super(webClient);
+		}
+		
+		@Override
+		public WebResponse getResponse(WebRequest request) throws IOException {
+			WebResponse response = super.getResponse(request);
+			if (response.getWebRequest().getUrl().toString().contains("session-timeout.js")) {
+				return createWebResponse(response.getWebRequest(), "", "application/javascript");
+			}
+			return super.getResponse(request);
+		}
 	}
 }
